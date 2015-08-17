ddda8b228cc344e3ce73ac7b9a295d830371ae3c
mariabelem <josepdark@gmail.com>
2015-08-16 22:22:15 -0500

    asign criteria

--- assets/js/controllers/showDisorderController.js
+++ assets/js/controllers/showDisorderController.js
@@ -3,6 +3,16 @@ angular.module('luria')
     console.log($stateParams);
     $scope.disorder = [];
     (function(){
+        reloadData();
+        $sails.on("disorder",function(message){
+          if(message.verb=="addedTo"){
+            reloadData();
+          }
+          console.log(message);
+        })
+    }());
+
+    function reloadData(){
       $sails.get("/disorder/"+$stateParams.disorderId)
         .success(function(data, status, headers, jwr){
           $scope.disorder = data;
@@ -10,7 +20,7 @@ angular.module('luria')
         .error(function(data, status, headers, jwr){
           throw new Error(data);
         });
-    }());
+    }
 
     $scope.showAddCriteriaDialog = function(ev){
       $mdDialog.show({
@@ -25,30 +35,53 @@ angular.module('luria')
           $scope.alert = 'Dialogo cancelado';
         });
     };
-  })
 
-  function AddCriteriaController($scope, $mdDialog, $http, $sails){
-    $scope.criteriaList = [];
-    (function(){
-      $sails.get("/criteria/")
-        .success(function(data, status, headers, jwr){
-          $scope.criteriaList = data;
-        })
-        .error(function(data, status, headers, jwr){
-          throw new Error(data);
-        });
-    }());
-    $scope.selectCriteria = function(criteria){
-      console.log(criteria);
-      $scope.selectedCriteria = criteria;
+    function AddCriteriaController($scope, $mdDialog, $http, $sails, $mdToast){
+      $scope.criteriaList = [];
+      (function(){
+        $sails.get("/criteria/")
+          .success(function(data, status, headers, jwr){
+            $scope.criteriaList = data;
+          })
+          .error(function(data, status, headers, jwr){
+            throw new Error(data);
+          });
+      }());
+      $scope.selectCriteria = function(criteria){
+        console.log(criteria);
+        $scope.selectedCriteria = criteria;
+      }
+
+      $scope.asignCriteria = function(){
+        if(!$scope.selectedCriteria){
+          $mdToast.showSimple('No as seleccionado un criterio');
+        }else{
+          var req = {
+            method: 'POST',
+            url:'/disorder/'+$stateParams.disorderId+'/criterion/'+$scope.selectedCriteria.id
+          };
+          $http(req)
+            .success(function(data){
+              $scope.disorder = data;
+              $mdDialog.cancel();
+            })
+            .error(function(data){
+              console.log(data);
+            })
+        }
+      }
+
+      $scope.createCriteria = function(newCriteria){
+
+      }
+      $scope.hide = function () {
+          $mdDialog.hide();
+      };
+      $scope.cancel = function () {
+          $mdDialog.cancel();
+      };
+      $scope.answer = function (answer) {
+          $mdDialog.hide(answer);
+      };
     }
-    $scope.hide = function () {
-        $mdDialog.hide();
-    };
-    $scope.cancel = function () {
-        $mdDialog.cancel();
-    };
-    $scope.answer = function (answer) {
-        $mdDialog.hide(answer);
-    };
-  }
+  })
